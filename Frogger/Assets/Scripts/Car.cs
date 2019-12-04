fe6e1a6628e6ad466c3604fe971d65417f39bd4d
Christian Simmons <csimmonscs@gmail.com>
2019-12-03 18:51:00 -0800

    Uploading Frogger to Github

---
+++ Frogger/Assets/Scripts/Car.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float minSpeed = 2f;
+    public float maxSpeed = 8f;
+
+    public GameObject car;
+
+    float speed = 1f;
+
+    private void Start()
+    {
+        speed = Random.Range(minSpeed, maxSpeed);
+    }
+
+    void FixedUpdate()
+    {
+        //transform.right
+        Vector2 forward = new Vector2(transform.right.x, transform.right.y);
+
+        rb.MovePosition(rb.position + forward * Time.fixedDeltaTime * speed);//moves the car to the right
+
+
+        Debug.Log(car.transform.position.x);
+        if (car.transform.position.x >= 15 || car.transform.position.x <= -15)
+        {
+            Destroy(car);
+        }
+    }
+}
---
+++ Frogger/Assets/Scripts/CarSpawner.cs
@@ -0,0 +1,48 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+
+    //random lane; spawn car
+
+    public float spawnDelay = 0.3f;
+
+    float nextTimeToSpawn = 0f;
+
+    public GameObject car;
+
+    //float countDownTimer = 3f;
+
+    public Transform[] spawnPoints;
+
+
+    private void Update()
+    {
+        //if (countDownTimer <= 0f)
+        //{
+        //    SpawnCar();
+        //    countDownTimer = 3f;
+        //}
+        //else
+        //{
+        //    countDownTimer -= Time.deltaTime;
+        //}
+
+        if(nextTimeToSpawn <= Time.time)
+        {
+            SpawnCar();
+            nextTimeToSpawn = Time.time + spawnDelay;
+        }
+    }
+
+    void SpawnCar()
+    {
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+
+        Transform spawnPoint = spawnPoints[randomIndex];
+
+        Instantiate(car, spawnPoint.position, spawnPoint.rotation);
+    }
+}
